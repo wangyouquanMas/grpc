a1ed5e852d3cd9057b4026adff2e0757b43fc466
wang <wang@wangdeMacBook-Pro.local>
2021-09-02 21:08:28 +0800

    update

--- day3/server.go
+++ day3/server.go
@@ -2,6 +2,7 @@ package day3
 
 import (
 	"errors"
+	"strings"
 	"sync"
 )
 
@@ -21,3 +22,27 @@ func (server *Server) Register(rcvr interface{}) error  {
 
 
 //服务发现
+func (server *Server) findService(serviceMethod string) (svc *service,mtype *methodType,err error){
+	//1 根据 服务名称 获取服务实例
+	dot :=strings.LastIndex(serviceMethod,".")
+	if dot<0{
+		errors.New("rpc server:service/method request ill-formed:"+serviceMethod)
+		return
+	}
+
+	serviceName,methodName := serviceMethod[:dot],serviceMethod[dot+1:]
+	svci,ok :=server.serviceMap.Load(serviceName)
+	if !ok{
+		err = errors.New("rpc server: can't find service"+serviceName)
+		return
+	}
+	//2 根据 方法名称 获取方法类型
+	 svc = svci.(*service)
+	 mtype = svc.method[methodName]
+	 if mtype == nil{
+	 	err = errors.New("rpc server: can't find method"+methodName)
+	 }
+	 return
+}
+
+
